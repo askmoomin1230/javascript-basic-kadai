9569b391cbec83267463bc7f2c3496428eabe10a
askmoomin1230 <144012465+askmoomin1230@users.noreply.github.com>
2023-10-11 11:16:02 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-window.addEventListener('mousedown', () => {
-  text.textContent = 'ボタンをクリックしました';
-})
-
