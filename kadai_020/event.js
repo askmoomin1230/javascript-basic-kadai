b8095f65c412c77084ae8e2d2aadf5a51aed64bb
askmoomin1230 <144012465+askmoomin1230@users.noreply.github.com>
2023-10-11 10:52:24 +0900

    kadai_020
    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+window.addEventListener('mousedown', () => {
+  text.textContent = 'ボタンをクリックしました';
+})
+
